42cbaac6267dc05583d98bee660f8aa5793138eb
Hasan Teke <hasan_t1998@hotmail.com>
2020-08-24 02:52:10 +0300

    adding slider conntrols to buttons

--- script.js
+++ script.js
@@ -20,9 +20,39 @@ var models = [{
     link: 'http://www.arabalar.com.tr/honda/civic/2019/1-6-elegance '
 }];
 
-var index = 1;
+var index = 0;
+var slaytCount = models.length;
 
-document.querySelector('.card-title').textContent = models[index].name;
-document.querySelector('.card-img-top').setAttribute('src', models[index].image);
-document.querySelector('.card-link').setAttribute('href', models[index].link);
+showSlide(index);
+
+document.querySelector('.fa-arrow-circle-left').addEventListener('click', function () {
+    index--;
+    showSlide(index);
+    console.log(index);
+});
+
+document.querySelector('.fa-arrow-circle-right').addEventListener('click', function () {
+    index++;
+    showSlide(index);
+    console.log(index);
+});
+
+function showSlide(i) {
+
+    index = i;
+
+    if (i < 0) {
+        index = slaytCount - 1;
+    }
+    if (i >= slaytCount) {
+        index = 0
+    }
+
+    document.querySelector('.card-title').textContent = models[index].name;
+    document.querySelector('.card-img-top').setAttribute('src', models[index].image);
+    document.querySelector('.card-link').setAttribute('href', models[index].link);
+}
+
+
+
 
